e38e708ca35c251d7e83f885e32ef689f17aafdd
baolongduong <68490958+baolongduong@users.noreply.github.com>
2021-12-04 18:08:58 +0700

    updated

--- THCN3/code/backend/Server/DB/Connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const URI = "mongodb+srv://dbHeartBeat:dbHeartBeat@cluster0.gdn2e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-const connectDB = async () =>{
-    await mongoose.connect(URI);
-    console.log('db connected...');
-};
-module.exports = connectDB;
